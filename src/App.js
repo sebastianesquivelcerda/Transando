46d60b3ceb46f5f04fd070a0982dbe593ef5ef5d
thomasenrique <79770143+thomasenrique@users.noreply.github.com>
2021-08-02 00:04:39 -0400

    fix tm

--- src/App.js
+++ src/App.js
@@ -2,37 +2,26 @@ import React, { Component } from 'react';
 import logo from './img/logo.svg';
 import './css/App.css';
 import './css/indexStyle.css';
-import SelectorDeWeones from './Componentes/MenuIndex';
 //import styles from './WpCrearPlantilla.module.scss';
 class App extends Component {
-
   render() {
     return (
-
       <div className="App">
-
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome cabro as l</h2>
         </div>
-
         <p className="App-intro"><h1>Inicio</h1></p>
-        <a href="page2.html">Entrar</a>
+        <a href="../public/page2.html">Entrar</a>
         {/* componente creado */}
         {/*      <SelectorDeWeones 
         urlNico = "nico.html"
         urlSeba = "seba.html"
         urlTom ="tom.html"
         /> */}
-
-
-
-
       </div>
     );
   }
-
-
 }
-
 export default App;
+
